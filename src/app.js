81c2c1e271df487b1ae2521184c67dd63a545731
Fernando Enrique Zepeda Castellanos <fernando.zepeda@seguritech.com>
2020-10-09 15:30:38 -0500

    post entpoint fix2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 var express = require('express')
 var app = express()
+const bodyParser = require('body-parser');
 
 const port = 3000
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+/* 
 app.get('/ejemplo', (req, res) => {
     res.status(200).send('Ok')
-})
+}) */
 app.post('/prueba', (req, res) => {
-    console.log("recibido: "+req.body);
+    console.log("objeto Recibido: ",req.body);
     res.status(200).json({
         message: 'Recibido exitosamente'
     })
