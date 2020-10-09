f94ac1e3b32ebb2007193025761af7676e762419
Fernando Enrique Zepeda Castellanos <fernando.zepeda@seguritech.com>
2020-10-09 15:11:37 -0500

    post entpoint

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ var app = express()
 
 const port = 3000
 
-app.get('/prueba', (req, res) => {
+app.get('/ejemplo', (req, res) => {
     res.status(200).send('Ok')
+})
+app.post('/prueba', (req, res) => {
+    console.log("recibido: "+req.body);
+    res.status(200).json({
+        message: 'Recibido exitosamente'
+    })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Ejecucion en: http://localhost:${port}`)
 })
